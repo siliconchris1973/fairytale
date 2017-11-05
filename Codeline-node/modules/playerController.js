0935044d60096f9d2c383468ca45dd26b277c6ea
Christian Günther <chris.guenther@mac.com>
2017-11-05 19:08:55 +0100

    removed old code directories

--- Codeline-node/modules/playerController.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var bodyParser = require('body-parser');
-router.use(bodyParser.urlencoded({ extended: true }));
-
-var player_routes = require('../routes/routes_player.js')
-
-module.exports = router;
