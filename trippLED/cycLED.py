5e69b92e665d91b9e6eed2ceabc575d763ed9d0e
Christian Günther <c.guenther@mac.com>
2017-11-19 18:37:21 +0100

    fixed typo in pin assignment

--- trippLED/cycLED.py
+++ trippLED/cycLED.py
@@ -15,9 +15,9 @@ GPIO.setup(GREENLED, GPIO.OUT) ## Setup GPIO Pin to OUT
 def Blink(pin, numTimes,speed):
     for i in range(0,numTimes):## Run loop numTimes
         print "Iteration " + str(i+1)## Print current loop
-        GPIO.output(7,True)## Switch on pin 7
+        GPIO.output(pin,True)## Switch on pin 7
         time.sleep(speed)## Wait
-        GPIO.output(7,False)## Switch off pin 7
+        GPIO.output(pin,False)## Switch off pin 7
         time.sleep(speed)## Wait
     print "Done" ## When loop is complete, print "Done"
     GPIO.cleanup()
@@ -25,7 +25,15 @@ def Blink(pin, numTimes,speed):
 ## Ask user for total number of blinks and length of each blink
 iterations = raw_input("Enter total number of times to blink: ")
 speed = raw_input("Enter length of each blink(seconds): ")
+led = raw_input("Enter color of led to blink (red|green|blue): ")
+
+if (led == "red"):
+    driveled = REDLED
+else if (led == "green"):
+    driveled = GREENLED
+else:
+    driveled = BLUELED
 
 ## Start Blink() function.
 ## Convert user input from strings to numeric data types and pass to Blink() as parameters
-Blink(int(BLUELED), int(iterations),float(speed))
+Blink(int(driveled), int(iterations),float(speed))
