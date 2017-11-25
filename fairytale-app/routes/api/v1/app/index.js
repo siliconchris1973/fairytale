66441cfc1e2e060bb79749ed59501be15ca1abee
Christian Günther <c.guenther@mac.com>
2017-11-25 11:45:21 +0100

    included endpoints for help and health in index.js

--- fairytale-app/routes/api/v1/app/index.js
+++ fairytale-app/routes/api/v1/app/index.js
@@ -4,11 +4,15 @@ const welcome = require('./welcome');
 const endpoints = require('./endpoints');
 const status = require('./status');
 const info = require('./info');
+const help = require('./help');
+const health = require('./health');
 
 app.get('/', welcome);
 app.get('/welcome', welcome);
 app.get('/endpoints', endpoints);
 app.get('/info', info);
 app.get('/status', status);
+app.get('/help', help);
+app.get('/health', health);
 
 module.exports = app;
